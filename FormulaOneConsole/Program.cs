d24ca32720bc17bbcb8de25794687b2e8a82cb6c
Valerio Palumbo <v.palumbo.1004@vallauri.edu>
2020-10-08 10:58:51 +0200

    Second commit

--- FormulaOneConsole/Program.cs
+++ FormulaOneConsole/Program.cs
@@ -1,12 +1,72 @@
 ﻿using System;
+using System.Data.SqlClient;
+using System.IO;
 
 namespace FormulaOneConsole
 {
     class Program
     {
+        public const string WORKINGPATH = @"C:\data\formulaone\";
+        private const string CONNECTION_STRING = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + WORKINGPATH + @"FormulaOne.mdf;Integrated Security=True";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("*** FORMULA ONE - BATCH OPERATIONS ***");
+            char scelta = ' ';
+            do
+            {
+                Console.WriteLine("\n*** FORMULA ONE - BATCH SCRIPTS ***\n");
+                Console.WriteLine("1 - Create Countries");
+                Console.WriteLine("2 - Create Teams");
+                Console.WriteLine("3 - Create Drivers");
+                Console.WriteLine("------------------");
+                Console.WriteLine("X - EXIT\n");
+                scelta = Console.ReadKey(true).KeyChar;
+                switch (scelta)
+                {
+                    case '1':
+                        ExecuteSqlScript("Countries.sql");
+                        break;
+                    case '2':
+                        ExecuteSqlScript("Teams.sql");
+                        break;
+                    case '3':
+                        ExecuteSqlScript("Drivers.sql");
+                        break;                   
+                    default:
+                        if (scelta != 'X' && scelta != 'x') Console.WriteLine("\nUncorrect Choice - Try Again\n");
+                        break;
+                }
+            } while (scelta != 'X' && scelta != 'x');
+        }
+
+        static void ExecuteSqlScript(string sqlScriptName)
+        {
+            var fileContent = File.ReadAllText(WORKINGPATH + sqlScriptName);
+            fileContent = fileContent.Replace("\r\n", "");
+            fileContent = fileContent.Replace("\r", "");
+            fileContent = fileContent.Replace("\n", "");
+            fileContent = fileContent.Replace("\t", "");
+            var sqlqueries = fileContent.Split(new[] { ";" }, StringSplitOptions.RemoveEmptyEntries);
+
+            var con = new SqlConnection(CONNECTION_STRING);
+            var cmd = new SqlCommand("query", con);
+            con.Open(); int i = 0; int nErr = 0;
+            foreach (var query in sqlqueries)
+            {
+                cmd.CommandText = query; i++;
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch (SqlException err)
+                {
+                    Console.WriteLine("Errore in esecuzione della query numero: " + i);
+                    Console.WriteLine("\tErrore SQL: " + err.Number + " - " + err.Message);
+                    nErr++;
+                }
+            }
+            string finalMessage = nErr == 0 ? "Script ended successfully without errors" : "Script ended with " + nErr + " errors";
+            Console.WriteLine(finalMessage);
         }
     }
 }
