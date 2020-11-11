cf32d554abf738951f3f584ce24bc131e882451e
PalumboValerio <61788962+PalumboValerio@users.noreply.github.com>
2020-11-11 17:48:58 +0100

    Backup and Restore

--- FormulaOneConsole/Program.cs
+++ FormulaOneConsole/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Data.SqlClient;
 using System.IO;
-using NormalExtension;
+using System.Threading;
+using ConsoleUtilities;
 
 namespace FormulaOneConsole
 {
@@ -10,13 +11,12 @@ namespace FormulaOneConsole
         public const string WORKINGPATH = @"C:\data\formulaone\";
         private const string CONNECTION_STRING = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + WORKINGPATH + @"FormulaOne.mdf;Integrated Security=True";
         public static string THISDATAPATH = $"{Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName}\\Data\\";
+        public static string DB ="[" + WORKINGPATH + "FormulaOne.mdf]";
 
         static void Main(string[] args)
         {
-            copySQLFiles(THISDATAPATH);
-            /*copyDB("Teams.sql");
-            copyDB("Drivers.sql");
-            copyDB("FK.sql");*/
+            Utilities utilities = new Utilities(WORKINGPATH, CONNECTION_STRING, THISDATAPATH, DB);
+            utilities.copySQLFiles(THISDATAPATH);
             char scelta = ' ';
             do
             {
@@ -35,140 +35,37 @@ namespace FormulaOneConsole
                 switch (scelta)
                 {
                     case '1':
-                        ExecuteSqlScript("Countries.sql");
+                        utilities.ExecuteSqlScript("Countries.sql");
                         break;
                     case '2':
-                        ExecuteSqlScript("Teams.sql");
+                        utilities.ExecuteSqlScript("Teams.sql");
                         break;
                     case '3':
-                        ExecuteSqlScript("Drivers.sql");
+                        utilities.ExecuteSqlScript("Drivers.sql");
                         break;
                     case '4':
-                        ExecuteSqlScript("FK.sql");
+                        utilities.ExecuteSqlScript("FK.sql");
                         break;
                     case 'a':
-                        Set();
+                        utilities.Set();
                         break;
                     case 'd':
-                        Drop();
+                        utilities.Drop();
                         break;
                     case 'r':
-                        ResetDB();
+                        utilities.ResetDB();
                         break;
                     default:
                         if (scelta != 'X' && scelta != 'x') Console.WriteLine("\nUncorrect Choice - Try Again\n");
                         break;
                 }
+                Thread.Sleep(2000);
+                Console.Clear();
             } while (scelta != 'X' && scelta != 'x');
         }
 
-        private static void copySQLFiles(string targetDirectory)
-        {
-            string[] fileEntries = Directory.GetFiles(targetDirectory);
-            foreach (string filePath in fileEntries)
-            {
-                string fileName = filePath.Split('\\').Last();
-                string newDbFilePath = WORKINGPATH + fileName;
-                string oldDbFilePath = filePath;
-                File.Copy(oldDbFilePath, newDbFilePath, true);
-            }           
-        }
+        
 
-        static bool ExecuteSqlScript(string sqlScriptName, bool reset = false)
-        {
-            bool error = true;
-            var fileContent = File.ReadAllText(WORKINGPATH + sqlScriptName);
-            fileContent = fileContent.Replace("\r\n", "");
-            fileContent = fileContent.Replace("\r", "");
-            fileContent = fileContent.Replace("\n", "");
-            fileContent = fileContent.Replace("\t", "");
-            var sqlqueries = fileContent.Split(new[] { ";" }, StringSplitOptions.RemoveEmptyEntries);
-
-            var con = new SqlConnection(CONNECTION_STRING);
-            var cmd = new SqlCommand("query", con);
-
-            con.Open(); int i = 0; int nErr = 0;
-            foreach (var query in sqlqueries)
-            {
-                cmd.CommandText = query; i++;
-                try
-                {
-                    cmd.ExecuteNonQuery();
-                }
-                catch (SqlException err)
-                {
-                    Console.WriteLine("Errore in esecuzione della query numero: " + i);
-                    Console.WriteLine("\tErrore SQL: " + err.Number + " - " + err.Message);
-                    nErr++;
-                    error = true;
-                }
-            }
-            con.Close();
-            string finalMessage = nErr == 0 ? "Script " + sqlScriptName + " ended without errors" : "Script ended with " + nErr + " errors";
-            if(!reset) Console.WriteLine(finalMessage);
-            return error;
-        }
-
-        private static void ExecuteQuery(string query, SqlConnection con)
-        {
-            var cmd = new SqlCommand(query, con);
-            cmd.ExecuteNonQuery();
-        }
-
-        private static void ResetDB()
-        {
-            BackupAndRestore("backup database [" + WORKINGPATH + "FormulaOne.mdf] to disk='{0}'", WORKINGPATH + "FormulaOneBackup.mdf", "Backup Created Sucessfully", "Backup Not Created");
-            try
-            {                
-                Drop();
-                Set();
-                Console.WriteLine("Reset concluso correttamente");
-            }
-            catch (Exception)
-            {
-                BackupAndRestore("restore database [" + WORKINGPATH + "FormulaOne.mdf] from disk='{0}'", WORKINGPATH + "FormulaOneBackup.mdf", "DB Restored Sucessfully", "DB Not Restored");
-            }
-        }
-
-        private static void Drop()
-        {
-            var con = new SqlConnection(CONNECTION_STRING);
-
-            con.Open();
-            ExecuteQuery("DROP TABLE IF EXISTS Country", con);
-            ExecuteQuery("DROP TABLE IF EXISTS Team", con);
-            ExecuteQuery("DROP TABLE IF EXISTS Driver", con);
-            con.Close();
-        }
-
-        private static void Set()
-        {
-            if (ExecuteSqlScript("Countries.sql", true)) throw new Exception("Error during set");
-            if (ExecuteSqlScript("Teams.sql", true)) throw new Exception("Error during set");
-            if (ExecuteSqlScript("Drivers.sql", true)) throw new Exception("Error during set");
-        }
-
-        private static void BackupAndRestore(string cmd, string db, string mex, string errMex)
-        {
-            try
-            {
-                using (SqlConnection conn = new SqlConnection(CONNECTION_STRING))
-                {
-                    string sqlStmt = string.Format(cmd, db);
-                    using (SqlCommand command = new SqlCommand(sqlStmt, conn))
-                    {
-                        conn.Open();
-                        command.ExecuteNonQuery();
-                        conn.Close();
-
-                        Console.WriteLine(mex);
-                    }
-                }
-            }
-            catch (Exception)
-            {
-                Console.WriteLine(errMex);
-            }
-        }
+        
     }
 }
---
+++ FormulaOneLibrary/Utilities.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.IO;
+using System.Data.SqlClient;
+using NormalExtension;
+
+namespace ConsoleUtilities
+{
+    public class Utilities
+    {
+        public string WORKINGPATH;
+        private string CONNECTION_STRING;
+        public string THISDATAPATH;
+        public string DB;
+        public Utilities(string wp, string cs, string tdp, string db) 
+        {
+            WORKINGPATH = wp;
+            CONNECTION_STRING = cs;
+            THISDATAPATH = tdp;
+            DB = db;
+        }
+
+        public void copySQLFiles(string targetDirectory)
+        {
+            string[] fileEntries = Directory.GetFiles(targetDirectory);
+            foreach (string filePath in fileEntries)
+            {
+                string fileName = filePath.Split('\\').Last();
+                string newDbFilePath = WORKINGPATH + fileName;
+                string oldDbFilePath = filePath;
+                File.Copy(oldDbFilePath, newDbFilePath, true);
+            }
+        }
+
+        public bool ExecuteSqlScript(string sqlScriptName, bool reset = false)
+        {
+            bool error = true;
+            var fileContent = File.ReadAllText(WORKINGPATH + sqlScriptName);
+            fileContent = fileContent.Replace("\r\n", "");
+            fileContent = fileContent.Replace("\r", "");
+            fileContent = fileContent.Replace("\n", "");
+            fileContent = fileContent.Replace("\t", "");
+            var sqlqueries = fileContent.Split(new[] { ";" }, StringSplitOptions.RemoveEmptyEntries);
+
+            var con = new SqlConnection(CONNECTION_STRING);
+            var cmd = new SqlCommand("query", con);
+
+            con.Open(); int i = 0; int nErr = 0;
+            foreach (var query in sqlqueries)
+            {
+                cmd.CommandText = query; i++;
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch (SqlException err)
+                {
+                    Console.WriteLine("Errore in esecuzione della query numero: " + i);
+                    Console.WriteLine("\tErrore SQL: " + err.Number + " - " + err.Message);
+                    nErr++;
+                    error = true;
+                }
+            }
+            con.Close();
+            string finalMessage = nErr == 0 ? "Script " + sqlScriptName + " ended without errors" : "Script ended with " + nErr + " errors";
+            if (!reset) Console.WriteLine(finalMessage);
+            return error;
+        }
+
+        public void ExecuteQuery(string query, SqlConnection con)
+        {
+            var cmd = new SqlCommand(query, con);
+            cmd.ExecuteNonQuery();
+        }
+
+        public void ResetDB()
+        {
+            Backup();
+            try
+            {
+                Drop();
+                Set();
+                Console.WriteLine("Reset concluso correttamente");
+            }
+            catch (Exception)
+            {
+                Restore();
+            }
+        }
+
+        public void Drop()
+        {
+            var con = new SqlConnection(CONNECTION_STRING);
+
+            con.Open();
+            ExecuteQuery("DROP TABLE IF EXISTS Country", con);
+            ExecuteQuery("DROP TABLE IF EXISTS Team", con);
+            ExecuteQuery("DROP TABLE IF EXISTS Driver", con);
+            con.Close();
+        }
+
+        public void Set()
+        {
+            if (ExecuteSqlScript("Countries.sql", true)) throw new Exception("Error during set");
+            if (ExecuteSqlScript("Teams.sql", true)) throw new Exception("Error during set");
+            if (ExecuteSqlScript("Drivers.sql", true)) throw new Exception("Error during set");
+        }
+
+        public void Backup()
+        {
+            try
+            {
+                using (SqlConnection dbConn = new SqlConnection())
+                {
+                    dbConn.ConnectionString = CONNECTION_STRING;
+                    dbConn.Open();
+
+                    using (SqlCommand multiuser_rollback_dbcomm = new SqlCommand())
+                    {
+                        multiuser_rollback_dbcomm.Connection = dbConn;
+                        multiuser_rollback_dbcomm.CommandText = @$"ALTER DATABASE {DB} SET MULTI_USER WITH ROLLBACK IMMEDIATE";
+
+                        multiuser_rollback_dbcomm.ExecuteNonQuery();
+                    }
+                    dbConn.Close();
+                }
+
+                SqlConnection.ClearAllPools();
+
+                using (SqlConnection backupConn = new SqlConnection())
+                {
+                    backupConn.ConnectionString = CONNECTION_STRING;
+                    backupConn.Open();
+
+                    using (SqlCommand backupcomm = new SqlCommand())
+                    {
+                        backupcomm.Connection = backupConn;
+                        backupcomm.CommandText = @$"BACKUP DATABASE {DB} TO DISK='{WORKINGPATH}FormulaOneBackup.bak'";
+                        backupcomm.ExecuteNonQuery();
+                    }
+                    backupConn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void Restore()
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(CONNECTION_STRING))
+                {
+                    con.Open();
+                    string sqlStmt2 = string.Format(@$"ALTER DATABASE {DB} SET SINGLE_USER WITH ROLLBACK IMMEDIATE");
+                    SqlCommand bu2 = new SqlCommand(sqlStmt2, con);
+                    bu2.ExecuteNonQuery();
+
+                    string sqlStmt3 = @$"USE MASTER RESTORE DATABASE {DB} FROM DISK='{WORKINGPATH}FormulaOneBackup.bak' WITH REPLACE;";
+                    SqlCommand bu3 = new SqlCommand(sqlStmt3, con);
+                    bu3.ExecuteNonQuery();
+
+                    string sqlStmt4 = string.Format(@$"ALTER DATABASE {DB} SET MULTI_USER");
+                    SqlCommand bu4 = new SqlCommand(sqlStmt4, con);
+                    bu4.ExecuteNonQuery();
+
+                    Console.WriteLine("C'è stato un problema con il Reset. Un backup del database è stato ripristinato");
+                    con.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
