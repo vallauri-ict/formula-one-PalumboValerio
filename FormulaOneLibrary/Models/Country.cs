ed8154c64906de961a5b657d408f0175f2176e0e
PalumboValerio <v.palumbo.1004@vallauri.edu>
2021-02-15 17:59:00 +0100

    Add WebServices

---
+++ FormulaOneLibrary/Models/Country.cs
@@ -0,0 +1,15 @@
+﻿  
+namespace ClassUtilities.Models
+{
+    public class Country
+    {
+        public Country(string countryCode, string countryName)
+        {
+            this.countryCode = countryCode;
+            this.countryName = countryName;
+        }
+
+        public string countryCode { get; set; }
+        public string countryName { get; set; }
+    }
+}
---
+++ FormulaOneWebServices/Controllers/CircuitController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClassUtilities;
+using System.IO;
+
+namespace FormulaOneWebServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CircuitController : ControllerBase
+    {
+        public const string WORKINGPATH = @"C:\data\formulaone\";
+        private const string CONNECTION_STRING = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + WORKINGPATH + @"FormulaOne.mdf;Integrated Security=True";
+        public static string THISDATAPATH = $"{Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName}\\Data\\";
+        public static string DB = "[" + WORKINGPATH + "FormulaOne.mdf]";
+        public Utilities utilities = new Utilities(WORKINGPATH, CONNECTION_STRING, THISDATAPATH, DB);
+
+        // GET: api/Country
+        [HttpGet]
+        public List<ClassUtilities.Models.Circuit> Get()
+        {
+            return utilities.getTableCircuit();
+        }
+
+        // GET: api/Country/5
+        [HttpGet("{id}")]
+        public ClassUtilities.Models.Circuit Get(int id)
+        {
+            return utilities.getTableCircuitByCode(id);
+        }
+
+        // POST: api/Country
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Country/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
